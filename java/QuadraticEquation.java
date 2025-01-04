ce8f5eab44902b6405ca80ede8f20f45e396234a
psk-1901 <sanjaykrishnapati@gmail.com>
2025-01-04 14:14:27 +0530

    added my website

---
+++ java/QuadraticEquation.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class QuadraticEquation {
+        public static void main(String[]Strings)
+    {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("enter the value of a:");
+            double a = input.nextDouble();
+            System.out.print("enter the value of b:");
+            double b = input.nextDouble();
+            System.out.print("enter the value of c:");
+            double c = input.nextDouble();
+            double d = b*b-4.0*a*c;
+            if(d>0.0)
+            {
+                double r1 = (-b+Math.pow(d,0.5))/(2.0*a);
+                double r2 = (-b-Math.pow(d,0.5))/(2.0*a);
+                System.out.println("the roots are"+r1+"and"+r2);
+            }
+            else if (d==0.0)
+            {
+                double r1 = -b/(2.0*a);
+                System.out.println("the root is"+r1);
+            }
+            else
+            {
+                System.out.println("roots are not real");
+            }
+        }
+        
+
+    }
+    
+}
---
+++ java/StringLiteralsExample.java
@@ -0,0 +1,10 @@
+public class StringLiteralsExample
+{
+ 	public static void main(String[] args)
+{
+        		String str1 = "Hello, World!";
+        		String str2 = "Java Programming";
+       		 System.out.println(str1);
+        		System.out.println(str2);
+    }
+}
